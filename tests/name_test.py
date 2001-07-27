92255d0313b397d95d62b54deda29d73f21b9b54
Monty Taylor <mordred@inaugust.com>
2001-07-27 12:47:55 +0000

    Minor mods.

--- tests/name_test.py
+++ tests/name_test.py
@@ -5,7 +5,7 @@ orb=CORBA.ORB_init([''],CORBA.ORB_ID)
 print orb
 name=orb.resolve_initial_references('NameService')._narrow(CosNaming.NamingContext)
 print name
-namelist=name.list(2)
+namelist=name.list(1000)
 print namelist
 for n in namelist[0]:
   print n.binding_name[0].id
