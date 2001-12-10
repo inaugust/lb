10b9e7a840426782dec94d3fbc4af76c76805da3
Monty Taylor <mordred@inaugust.com>
2001-12-10 17:16:08 +0000

    Found the real way to fix the problem. Setting parser.returns_unicode=0 disables the unicode stuff.

--- lib/python/ExpatXMLParser.py
+++ lib/python/ExpatXMLParser.py
@@ -21,6 +21,7 @@ class ExpatXMLParser:
         self.parser.StartElementHandler = self.magic_start_element
         self.parser.EndElementHandler = self.magic_end_element
         self.parser.CharacterDataHandler = self.char_data
+        self.parser.returns_unicode=0
         self.Parse=self.parser.Parse
 
     def unknown_starttag(self, name, attrs):
@@ -56,19 +57,21 @@ class ExpatXMLParser:
 
 class DOMNode:
     def __init__(self, tag=None, attrs={}):
-        self.tag=str(tag)
+##        self.tag=str(tag)
+        self.tag=tag
         self.attrs = attrs
-        for k,v in self.attrs.items():
-            try:
-                if type(k)==type(u'a'):
-                    del(self.attrs[k])
-                    k=str(k)
-                if type(v)==type(u'a'):
-                    self.attrs[k]=str(v)
-            except:
-                # Pre 2.1 we should get syntax error. But that's ok.
-                # because pre 2.1 we don't need to do this.
-                pass
+##        for k,v in self.attrs.items():
+##             try:
+##                 if type(k)==type(u'a'):
+##                     del(self.attrs[k])
+##                     k=str(k)
+##                 if type(v)==type(u'a'):
+##                     self.attrs[k]=str(v)
+##             except:
+##                 # Pre 2.1 we should get syntax error. But that's ok.
+##                 # because pre 2.1 we don't need to do this.
+##                 pass
+            
         self.data = ''
         self.children=[]
         
@@ -77,11 +80,11 @@ class DOMNode:
             self.children.append(child)
 
     def add_data(self, data):
-        try:
-            if type(data)==type(u'a'):
-                data=str(data)
-        except:
-            pass #Python pre-2.1 syntax error above. But that's ok.
+##         try:
+##             if type(data)==type(u'a'):
+##                 data=str(data)
+##         except:
+##             pass #Python pre-2.1 syntax error above. But that's ok.
         
         self.data = self.data + data
 
